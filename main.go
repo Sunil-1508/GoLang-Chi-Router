beb5cbbd066a58037bb588a6c15a04a25863f264
Sunil-1508 <20a91a05b1@aec.edu.in>
2024-02-02 15:59:37 +0530

    added routes

--- main.go
+++ main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
+	router := chi.NewRouter()
+	router.Get("/",homehandler)
+	router.Get("/hello", basichandler)
 	server := &http.Server{
 		Addr:    ":3000",
-		Handler: http.HandlerFunc(basichandler),
+		Handler: router,
 	}
 
 	err := server.ListenAndServe()
@@ -16,7 +21,9 @@ func main() {
 		fmt.Println("failed to Listen to the port", err)
 	}
 }
-
+func homehandler(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("home page"))
+}
 func basichandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello world !!!!!"))
+	w.Write([]byte("Hello World !"))
 }
